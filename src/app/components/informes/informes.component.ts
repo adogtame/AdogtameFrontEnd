645fbd2bbe362371344187dc23eb677fc6d73d88
adogtame <83562485+adogtame@users.noreply.github.com>
2021-10-28 15:56:39 -0300

    correcion en informes. Ahora cargan luego de apretar el boton

--- src/app/components/informes/informes.component.ts
+++ src/app/components/informes/informes.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UsuariosService } from '../../services/usuarios.service';
-
 import { ChartType, ChartOptions, ChartDataSets } from 'chart.js';
 import { SingleDataSet, Label, monkeyPatchChartJsLegend, monkeyPatchChartJsTooltip } from 'ng2-charts';
+
 @Component({
   selector: 'app-informes',
   templateUrl: './informes.component.html',
@@ -23,7 +23,6 @@ export class InformesComponent implements OnInit {
   ) {
     monkeyPatchChartJsTooltip();
     monkeyPatchChartJsLegend();
-    
   }
 
   ngOnInit(): void {
@@ -32,31 +31,39 @@ export class InformesComponent implements OnInit {
     this.cantidadUsuariosRegistrados();
     this.cantidadAnimalesEnAdopcion();
     this.promedioAnimalesAdoptados();
-
-
   }
 
-  /* Grafico de torta
 
-    public pieChartOptions: ChartOptions = {
-      responsive: true,
-    };
-    public pieChartLabels: Label[] = [['Adoptados'], [ 'Sin Adoptar']];
-    public pieChartData: SingleDataSet = [ , ]; // tomo el valor dentro de una funcion de mas abajo para que guarde el valor
-    public pieChartType: ChartType = 'pie';
-    public pieChartLegend = true;
-    public pieChartPlugins = [];
-  */
-
- barChartOptions: ChartOptions = {
+  public barChartOptions: ChartOptions = {
     responsive: true,
+    scales: { xAxes: [{}], yAxes: [{}] },
+    plugins: {
+      datalabels: {
+        anchor: 'end',
+        align: 'end',
+      }
+    }
   };
- barChartLabels: Label[] = ['Animales registrados', 'Animales Adoptados', 'Animales sin Adoptar', 'Usuarios Registrados', 'Promedo de Animales Adoptados'];
- barChartType: ChartType = 'bar';
- barChartLegend = true;
- barChartPlugins = [];
 
- barChartData: ChartDataSets[] = [{ data: [ , , , , ], label: 'Cantidad' }];
+  public barChartLabels: Label[] = ['Animales Registrados', 'Animales Adoptados', 'Animales en Adopcion', 'Usuarios Registrados', 'Promedio de Adoptados'];
+  public barChartType: ChartType = 'bar';
+  public barChartLegend = true;
+  public barChartData: ChartDataSets[] = [{ data: [, , , , , ,], label: 'Cantidad' }]; // Lo pongo vacio porque todavia no tengo los datos
+
+
+  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public randomize(): void {
+    // Cargo los valores despues que los cargue en el init con el suscribe
+    this.barChartData = [{ data: [this.cantidadAnimalesRegistrado.an, this.cantidadAnimalesAdoptado.ana, this.cantidadAnimalesAdopcion.ane, this.cantidadUsuariosRegistrado.us, this.promedioAnimalesAdoptado.prom], label: 'Cantidad' }];
+  }
+
 
 
   cantidadUsuariosRegistrados(): void {
@@ -112,10 +119,7 @@ export class InformesComponent implements OnInit {
         this.promedioAnimalesAdoptado = res;
         console.log(res);
         console.log("promedio adoptado", this.promedioAnimalesAdoptado);
-        this.promedioAnimalesAdoptado.prom = Math.round(this.promedioAnimalesAdoptado.prom * 1000)/1000;
-        // Aca guardo los valores del grafico, si lo pongo por fuera me trae undefined
-        //this.pieChartData = [this.cantidadAnimalesAdoptado.ana, this.cantidadAnimalesAdopcion.ane];
-        this.barChartData = [{ data: [this.cantidadAnimalesRegistrado.an, this.cantidadAnimalesAdoptado.ana, this.cantidadAnimalesAdopcion.ane, this.cantidadUsuariosRegistrado.us, this.promedioAnimalesAdoptado.prom], label: 'Cantidad' }];
+        this.promedioAnimalesAdoptado.prom = Math.round(this.promedioAnimalesAdoptado.prom * 1000) / 1000;
       },
       err => console.log(err)
     )
